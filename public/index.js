deb90fc4b9a5daadf74ed275c4b4f5b1586cfb24
EDA <eda@mac-mini-3.local>
2016-09-27 15:14:29 +1300

    started index.js file

---
+++ public/index.js
@@ -0,0 +1,12 @@
+
+
+var url = document.getElementById("url")
+var urlRender = document.getElementById("urlRender")
+
+function renderUrl () = {
+  var url = document.getElementById("url").value
+  var iframe = document.getElementById("urlRender")
+}
+
+//Take the url in id
+//When the button is clicked, put the url into the iFrame code in urlRender
