2d4be382423888c7bae44f4055680b202756e4e4
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2016-09-27 21:28:05 +1300

    index.js to have jQuery for url button

--- public/index.js
+++ public/index.js
@@ -1,12 +1,23 @@
 
+//
+// var url = document.getElementById("url")
+// var urlRender = document.getElementById("urlRender")
 
-var url = document.getElementById("url")
-var urlRender = document.getElementById("urlRender")
-
-function renderUrl () = {
-  var url = document.getElementById("url").value
-  var iframe = document.getElementById("urlRender")
-}
+// function renderUrl() = {
+//   var url = document.getElementById("url").value;
+//   document.getElementById("urlRender").src = url;
+// }
 
 //Take the url in id
 //When the button is clicked, put the url into the iFrame code in urlRender
+
+
+// function myFunction() {
+//     var x = document.getElementById("myURL").value;
+//     document.getElementById("demo").innerHTML = x;
+// }
+
+
+function renderUrl(){
+    $('#urlRender').attr('src', $('#url').val());
+}
