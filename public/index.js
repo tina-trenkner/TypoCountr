2ff9ebf3ed574929ca5161afafc6c0e8d0a51bae
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2016-09-28 20:48:06 +1300

    added count function

--- public/index.js
+++ public/index.js
@@ -3,6 +3,18 @@ function renderUrl(){
       browserFrame.src= document.getElementById("txtUrl").value;
   }
 
+function modifyCount(val) {
+    var qty = document.getElementById("count").value;
+    var new_qty =  parseInt(qty, 10) + val;
+
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+
+    document.getElementById("count").value = new_qty;
+    return new_qty;
+}
+
 
 
 // var url = document.getElementById("url")
