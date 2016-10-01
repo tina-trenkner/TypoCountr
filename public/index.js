d6c58e040cbb791eece327e5a41d133f476b3a4d
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2016-10-01 21:45:13 +1300

    can console log most of the URL string

--- public/index.js
+++ public/index.js
@@ -1,44 +1,51 @@
 function renderUrlButton(){
-      var browserFrame = document.getElementById("browser");
-      browserFrame.src= document.getElementById("txtUrl").value;
+    var browserFrame = document.getElementById("browser")
+    browserFrame.src = document.getElementById("txtUrl").value;
+    console.log(browserFrame.src)
+    return browserFrame.src
 }
 
 function renderUrlEnter(){
     var txt = document.getElementById("txtUrl");
     txt.addEventListener("keypress", function(event) {
       if (event.keyCode === 13)
-        renderUrlButton();
+        renderUrlButton()
     })
 }
 
 function modifyCount(val) {
-    var qty = document.getElementById("count").value;
-    var new_qty =  parseInt(qty,10) + val;
+  var qty = document.getElementById("count").value;
+  var new_qty =  parseInt(qty,10) + val;
 
     if (new_qty < 0) {
         new_qty = 0;
     }
 
     document.getElementById("count").value = new_qty;
+    console.log(new_qty);
     return new_qty;
 }
+
+
+function tweetUrl() {
+  return "https://twitter.com/intent/tweet?text=I%20found%20typos%20in%20this%20story%20&url=http%3A%2F%2F" + "www.washingtonpost.com" + "%2F&hashtags=typocountr"
+}
+console.log(tweetUrl())
+
+//document.getElementById("tweet").href = tweetUrl()
+
+
+// https://twitter.com/intent/tweet?text=I%20found%20typos%20in%20this%20story%20&url=http%3A%2F%2Fwww.washingtonpost.com%2F&hashtags=typocountr
+
 
 // function tweetTypo () {
 //   var iframe = document.getElementById("browser")
 //   console.log(iframe.contentWindow.location.href);
 // }
 
-// function tweetTypo() {
+
+// function createTweetUrl () {
 //   var count = document.getElementById("count").value;
 //   var url = document.getElementById("txtUrl").value;
-//   var tweetButton = document.getElementById("tweetButton")
-//
-//   tweetButton.addEventListener("click", function(event) {
-//      Bring up Twitter window
-//      Make sure that window is prepopulated with count and link
-//   })
-
-  //Throw those two values into a prepopulated tweet: "I found TK typos in this story [link] #typocountr"
-// }
-//
-// 'https://twitter.com/intent/tweet?text=I%20found' + count + ''%20typos%20in%20this%20story%20' + url + '?hashtags=typocountr'
+//   var tweetUrl = "https://twitter.com/intent/tweet?text=I%20found%20" + count + "%20in%20this%20story%20" + url + " &?hashtags=typocountr"
+//}
